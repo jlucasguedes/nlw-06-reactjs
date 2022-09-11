7f2e9b4ea08353d1e65a5a736f3ef9ff60c3a73e
João Lucas Guedes de Oliveira <iftolucas@gmail.com>
2022-09-11 08:16:49 -0300

    Aula 01 - criando estrutura do projeto

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/Button";
+
+function App() {
+  return (
+    <div>
+      <Button text="Clique ali" />
+      <Button text="Clique ali" />
+      <Button text="Clique ali" />
+      <Button text="Clique ali" />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
